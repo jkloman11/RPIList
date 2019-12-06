b0a58747f2221217289d7f377ae2bb6605f99dff
jkloman11 <jkloman11@gmail.com>
2019-12-05 22:58:46 -0500

    major site cleanup and added IA

---
+++ home/home.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+   // example event handler:
+   $('#goNext').click(function() {
+      
+   });
+   $('.job').click(function () {
+      
+   });
+   
+   $('#goBack').click(function(){
+      if(document.getElementById('page')=='1'){
+         document.getElementById("goBack").disabled = true;
+      }
+   })
+                       
+                       
+});
---
+++ resources/site.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    htmlStr = `
+        <h1><a href = "/RPIList/home">RPI List</a></h1>
+        <ul class = "navBar">
+            <li><a href = "/RPIList/create-post">Create a Post</a></li>
+            <li><a href = "/RPIList/profile"> Profile</a></li>
+        </ul>
+    `
+    document.getElementById('header').innerHTML = htmlStr;
+});
